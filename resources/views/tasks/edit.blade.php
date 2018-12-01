c1a3a0eadaac6cad6a091f233391189c20abf375
Ocea Knw <taro.kirameki@techacademy.jp>
2018-12-01 14:33:16 +0000

    CRUD tasks

---
+++ resources/views/tasks/edit.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>id: {{ $tasks->id }}の編集ページ</h1>
+    
+    {!! Form::model($tasks,['route'=> ['tasks.update', $task->id], 'method' => 'put'])!!}
+    
+         {!! Form::label('content', 'タスク:') !!}
+         {!! Form::text('content') !!}
+         
+         {!! Form::submit('更新') !!}
+         
+    {!! Form::close() !!}
+
+@endsection
---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>id = {{$task->id}}の詳細ページ/h1>
+    <p>{{$task->content}}</p>
+    
+    {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id]) !!}
+    
+    {!! Form::model($tasks, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除') !!}
+
+@endsection
